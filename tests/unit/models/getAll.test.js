91d90e3a3d7d4d0698c69fb3a601b0dfe749d409
vinicius-vasconcelos <viniciussouzav@gmail.com>
2022-10-25 15:57:13 -0300

    Descomentando testes

--- tests/unit/models/getAll.test.js
+++ tests/unit/models/getAll.test.js
@@ -1,14 +1,14 @@
-// const { expect } = require('chai');
-// const model = require('../../../src/models/user');
+const { expect } = require('chai');
+const model = require('../../../src/models/user');
 
-// describe('Realizando testes de modelo de usuário', function() {
-//     it('Deve-ser retornar um array', async function() {
-//         const result = await model.getAll();
-//         expect(result).to.be.an('array');
-//     });
+describe('Realizando testes de modelo de usuário', function() {
+    it('Deve-ser retornar um array', async function() {
+        const result = await model.getAll();
+        expect(result).to.be.an('array');
+    });
 
-//     it('O array não pode estar vázio', async function() {
-//         const result = await model.getAll();
-//         expect(result).to.be.not.empty;
-//     });
-// });
+    it('O array não pode estar vázio', async function() {
+        const result = await model.getAll();
+        expect(result).to.be.not.empty;
+    });
+});
--- tests/unit/services/getAll.test.js
+++ tests/unit/services/getAll.test.js
@@ -1,26 +1,26 @@
-// const { expect } = require('chai');
-// const service = require('../../../src/services/user');
-// const model = require('../../../src/models/user');
-// const sinon = require('sinon');
+const { expect } = require('chai');
+const service = require('../../../src/services/user');
+const model = require('../../../src/models/user');
+const sinon = require('sinon');
 
-// describe('Realizando testes de serviço de usuário', function() {
-//     it('Deveria retornar um array', async function() {
-//         const output= [
-//             'Vinicius',
-//             'Fabison',
-//             'Angélica'
-//         ];
+describe('Realizando testes de serviço de usuário', function() {
+    it('Deveria retornar um array', async function() {
+        const output= [
+            'Vinicius',
+            'Fabison',
+            'Angélica'
+        ];
 
-//         sinon.stub(model, 'getAll').resolves(output);
+        sinon.stub(model, 'getAll').resolves(output);
           
-//         const result = await service.getAll();
-//         expect(result).to.be.an('array');
-//         expect(result).to.be.deep.equal([
-//             'Vinicius',
-//             'Fabison',
-//             'Angélica'
-//         ])
+        const result = await service.getAll();
+        expect(result).to.be.an('array');
+        expect(result).to.be.deep.equal([
+            'Vinicius',
+            'Fabison',
+            'Angélica'
+        ])
 
-//         sinon.restore();
-//     });
-// });
+        sinon.restore();
+    });
+});
